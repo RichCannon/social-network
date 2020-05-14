d651fbfa742a76c83a229faf680cf8394d75475d
RichCannon <syndicateua@gmail.com>
2020-05-14 10:24:54 +0300

    added basename for BrowserRouter

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
         }
 
         return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div className='app-wrapper'>
                     <HeaderContainer/>
                     <Navbar/>
